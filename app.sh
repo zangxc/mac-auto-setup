dd0f49f5d705d7517996d44fe9ade81b15c4ab47
zangxc <zangxuecong@gmail.com>
2023-04-06 10:53:38 +1000

    Update app.sh

--- app.sh
+++ app.sh
@@ -14,26 +14,26 @@ EOS
 echo " ----- Install web apps ------"
 sudo softwareupdate --install-rosetta
 
-brew install --cask 1password
-brew tap AdoptOpenJDK/openjdk
-brew install --cask adoptopenjdk
-brew install --cask android-studio
+brew install --cask keepassxc
+# brew tap AdoptOpenJDK/openjdk
+# brew install --cask adoptopenjdk
+# brew install --cask android-studio
 brew install --cask docker
 brew install --cask google-chrome
 brew install --cask iterm2
-brew install --cask google-japanese-ime
+# brew install --cask google-japanese-ime
 brew install --cask slack
-brew install --cask spotify
-brew install --cask imageoptim
+# brew install --cask spotify
+# brew install --cask imageoptim
 brew install --cask visual-studio-code
-brew install --cask adobe-creative-cloud
+# brew install --cask adobe-creative-cloud
 brew install --cask gyazo
 brew install --cask zoom
-brew install --cask discord
+# brew install --cask discord
 brew install --cask notion
-brew install --cask figma
-brew install --cask tableplus
-brew install --cask postman
+# brew install --cask figma
+# brew install --cask tableplus
+# brew install --cask postman
 brew install --cask clipy
 brew install --cask alfred
 
@@ -43,8 +43,8 @@ case $input in
   echo "Skip"
   ;;
 [Yy]*)
-  brew install --cask microsoft-office
-  brew install --cask intellij-idea
-  brew install --cask clip-studio-paint
+#   brew install --cask microsoft-office
+#   brew install --cask intellij-idea
+#   brew install --cask clip-studio-paint
   ;;
 esac
